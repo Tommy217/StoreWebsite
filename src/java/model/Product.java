737c1a688aec0d56ac1ef13f9d358754df5668fd
MINTUW\FPT <tranducthangbk2k5@gmail.com>
2025-09-27 22:05:55 +0700

    init productDAO and categoryDAO

---
+++ src/java/model/Product.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author FPT
+ */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    private int id;
+    private String name;
+    private String img;
+    private int quantity;
+    private float price;
+    private String description;
+    private int categoryId;
+}
